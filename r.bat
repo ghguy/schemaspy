8ca56dcd625819e08c00fc719a2ae4e6b6295494
ghguy <jamestanms@hotmail.com>
2018-09-05 10:02:24 +0800

    Updated SchemaSpy to version 6

--- r.bat
+++ r.bat
@@ -1,7 +1,8 @@
 rem https://graphviz.gitlab.io/_pages/Download/Download_windows.html
 set PATH=C:\Program Files (x86)\Graphviz2.38\bin;%PATH%
 
-java -jar .\schemaSpy_5.0.0.jar -dp .\mssql-jdbc-7.0.0.jre8.jar -t mssql05 -host 137.132.1.204 -port 62242 -db HanshanERS_TEST -s dbo -u sa -p h@nsh@n -o erstest
+java -jar .\schemaspy-6.0.0.jar -dp .\mssql-jdbc-7.0.0.jre8.jar -t mssql05 -host 137.132.1.204 -port 62242 -db HanshanERS_TEST -s dbo -u sa -p h@nsh@n -o erstest
 
+start erstest\index.html
 
 pause
